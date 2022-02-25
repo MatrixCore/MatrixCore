2a6d7d886e1f507cea89be4b905e207e21e770dc
Finn Behrens <me@kloenk.dev>
2022-02-25 18:25:09 +0100

    implement login

--- Sources/mcc/LoginCommand.swift
+++ Sources/mcc/LoginCommand.swift
@@ -8,6 +8,7 @@
 import Foundation
 import ArgumentParser
 import MatrixClient
+import os
 
 struct LoginCommand: ParsableCommand {
     static var configuration = CommandConfiguration(
@@ -21,9 +22,24 @@ struct LoginCommand: ParsableCommand {
     @Option(name: .shortAndLong, help: "The userid to use to login")
     var userID: String
     
+    @Option(name: .shortAndLong, help: "Password to log in with")
+    var password: String
+    
+    @Option(name: .shortAndLong, help: "Device id to set")
+    var deviceId: String?
+    
     func run() async throws {
+        let logger = Logger.init()
         let client = await MatrixClient.init(homeserver: try MatrixHomeserver.init(resolve: homeserver))
         
-        print("resolved url: \(client.homeserver)")
+        logger.debug("resolved url: \(client.homeserver.url)")
+        
+        let info = try await client.getVersions()
+        logger.debug("server has versions: \(info.versions)")
+        
+        let login = try await client.login(username: userID, password: password, deviceId: deviceId)
+        print("user id: \(login.userId ?? "")")
+        print("token: \(login.accessToken ?? "")")
+        print("device id: \(login.deviceId ?? "")")
     }
 }
