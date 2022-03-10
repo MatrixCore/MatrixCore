545189d75e252cfead1cf94ef2aee7d04884cadc
Finn Behrens <me@kloenk.dev>
2022-03-10 15:28:53 +0100

    Remove mcc
    LINK: https://github.com/MatrixCore/MCC

--- Package.swift
+++ Package.swift
@@ -22,8 +22,6 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "AnyCodable", url: "https://github.com/Flight-School/AnyCodable", from: "0.6.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", branch: "async"),
-        .package(url: "https://github.com/httpswift/swifter.git", .upToNextMajor(from: "1.5.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -41,13 +39,5 @@ let package = Package(
         .testTarget(
             name: "MatrixCoreTests",
             dependencies: ["MatrixCore"]),
-        .executableTarget(
-                name: "mcc",
-                dependencies: [
-                    "MatrixClient",
-                    .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                    .product(name: "Swifter", package: "swifter")
-                ]
-        )
     ]
 )
